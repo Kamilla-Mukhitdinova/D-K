e166087ebc7be46bfc43e11503162ed1d62a4b87
Kamilla <muhitdinovakamilla10@gmail.com>
2026-03-06 16:34:09 +0500

    fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "D-K",
+  base: "/D-K/",
   server: {
     host: "::",
     port: 8080,
